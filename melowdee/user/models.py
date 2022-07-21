80e69dd27ab8491da33e0222fd737a33c3709a3a
Ryan Arj <ryanarj@Ryans-MBP-2.home>
2022-07-21 16:42:49 -0400

    update user meta

--- melowdee/user/models.py
+++ melowdee/user/models.py
@@ -6,5 +6,6 @@ class UserMetadata(models.Model):
     user_name = models.CharField(max_length=30)
     age = models.IntegerField()
     email = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
