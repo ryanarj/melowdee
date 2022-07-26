ba74ab3cec7f9a253ff1ea74efe7cecf2ab0bf58
Ryan Arj <ryanarj@Ryans-MBP-2.home>
2022-07-26 15:20:02 -0400

    cors lite

--- melowdee/settings.py
+++ melowdee/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'melowdee',
     'melowdee.user',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'melowdee.urls'
@@ -135,3 +138,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     )
 }
+
+CORS_ALLOWED_ORIGINS  = [
+    "http://localhost:3000",
+]
