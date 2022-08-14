3e4153f8e94e93faa67e1518db6597b9c416a1f5
ryanarj <ryanarj07@gmail.com>
2022-08-14 17:34:09 -0400

    add sigin user test

--- melowdee/auth/user/tests/test_user.py
+++ melowdee/auth/user/tests/test_user.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
-from melowdee.auth.user.views import user_signup
+from melowdee.auth.user.views import user_signup, user_sign_in
 
 
 class UserTestCase(TestCase):
@@ -16,7 +16,28 @@ class UserTestCase(TestCase):
             'age': 1,
             'password': 'test'
         }, format='json')
-        user_signup(request)
+        response = user_signup(request)
 
         user = User.objects.filter(username=test_username, email=test_email)
         assert user.exists() is True
+        assert response.status_code == 201
+
+    def test_user_user_sign_in(self):
+        factory = APIRequestFactory()
+        test_username = 'Test23'
+        test_email = 'Test23@melowdee.com'
+
+        request = factory.post('/user_signup/', {
+            'username': test_username,
+            'email': test_email,
+            'age': 1,
+            'password': 'test'
+        }, format='json')
+        user_signup(request)
+
+        request = factory.post('/user_sign_in/', {
+            'email': test_email,
+            'password': 'test'
+        }, format='json')
+        response = user_sign_in(request)
+        assert response.status_code == 201
