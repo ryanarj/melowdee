e2b0dab9cd7dd2794d69e311b4f4c49a236a3ffd
ryanarj <ryanarj07@gmail.com>
2022-08-16 19:37:10 -0400

    more tests

---
+++ melowdee/core/artist/tests/test_artist.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+
+from melowdee.core.artist.models import Artist
+from melowdee.core.artist.views import add_artist, all_artists
+
+
+class ArtistTestCase(TestCase):
+
+    def test_user_signup(self):
+        factory = APIRequestFactory()
+        artist = 'The Strokes'
+        about = 'Greatness'
+        request = factory.post('/add_artist/', {
+            'name': artist,
+            'about': about,
+        }, format='json')
+        response = add_artist(request)
+
+        user = Artist.objects.filter(name=artist)
+        assert user.exists() is True
+        assert user.count() == 1
+        assert response.status_code == 201
+
+    def test_user_user_sign_in(self):
+        factory = APIRequestFactory()
+        artist = 'Biggie Smalls'
+        about = 'Greatness'
+
+        request = factory.post('/add_artist/', {
+            'name': artist,
+            'about': about,
+        }, format='json')
+        add_artist(request)
+
+        request = factory.get('/all_artists/', format='json')
+        response = all_artists(request)
+
+        assert response.status_code == 200
+        assert artist in str(response._container)
