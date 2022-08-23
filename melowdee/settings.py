a0dc7df90859de389ea8aac7ff856f883fee99d0
ryanarj <ryanarj07@gmail.com>
2022-08-23 18:03:55 -0400

    redis

--- melowdee/settings.py
+++ melowdee/settings.py
@@ -151,3 +151,14 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ARTISTS_PER_PAGE = 10
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
