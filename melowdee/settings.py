cfa811fa72545845f9c0851b5c7ed3d61e583eb0
ryanarj <ryanarj07@gmail.com>
2022-09-14 14:34:33 -0400

    clean up

--- melowdee/settings.py
+++ melowdee/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework_swagger',
     'drf_yasg',
     'melowdee',
     'melowdee.auth.user',
