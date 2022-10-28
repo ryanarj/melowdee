4ee1e92055c1e091d35d0695011b569c1b2dbaad
ryanarj <ryanarj07@gmail.com>
2022-10-28 16:25:35 -0400

    skeleton for checkbalance

--- melowdee/core/wallet/serializer.py
+++ melowdee/core/wallet/serializer.py
@@ -25,3 +25,25 @@ class WalletSerializer(serializers.Serializer):
                     user=user, public=data.get('public'), private=data.get('private'), address=data.get('address')
                 )
                 return wallet
+
+
+class BalanceSerializer(serializers.Serializer):
+    user_id = serializers.CharField(allow_null=False)
+
+
+    def create(self, validated_data):
+        user_id = validated_data.get('user_id')
+
+        wallet_q = Wallet.objects.filter(user_id=user_id)
+        #
+        # if not wallet_q:
+        #     user = User.objects.filter(id=user_id).first()
+        #     data = asyncio.run(generate_wallet(user.password))
+        #
+        #     with transaction.atomic():
+        #         wallet = Wallet.objects.create(
+        #             user=user, public=data.get('public'), private=data.get('private'), address=data.get('address')
+        #         )
+        #         return wallet
+        else:
+            print('Need a a wallet')
--- melowdee/external/wallet_service.py
+++ melowdee/external/wallet_service.py
@@ -9,3 +9,11 @@ async def generate_wallet(password):
         async with session.post(url=url, data={'password': password}) as resp:
             body = await resp.json()
             return body
+
+
+async def check_balance(address):
+    async with aiohttp.ClientSession() as session:
+        url = 'http://127.0.0.1:8080/checkBalance'
+        async with session.post(url=url, data={'address': address}) as resp:
+            body = await resp.json()
+            return body
